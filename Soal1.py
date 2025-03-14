982c268ac05969d92cf1adff3dd344dbd85592a7
sandy29 <sandylinta8@gmail.com>
2025-03-14 14:56:03 +0800

    Soal1.py

--- Soal1.py
+++ Soal1.py
@@ -1,1 +1,11 @@
+def perkalian(x, y):
+    # Menghitung hasil perkalian
+    hasil_perkalian = x*y
 
+    # Menampilkan hasil
+    print(f"(x) x (y) = ", end="")
+    print(" + ".join([str(y)] * x), end=" = ")
+    print(hasil_perkalian)
+
+perkalian(6,5)
+perkalian(7,10)
